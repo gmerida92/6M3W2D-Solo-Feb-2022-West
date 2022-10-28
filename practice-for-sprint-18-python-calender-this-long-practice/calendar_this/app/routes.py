ef8791e50503a77705862a1c33e2524cc1ca88cb
George Merida <gmerida92@gmail.com>
2022-10-27 18:42:52 -0700

    Caught up to Wednesday

--- practice-for-sprint-18-python-calender-this-long-practice/calendar_this/app/routes.py
+++ practice-for-sprint-18-python-calender-this-long-practice/calendar_this/app/routes.py
@@ -1,8 +1,27 @@
+from datetime import datetime
 from flask import (Blueprint, render_template)
+import os
+import sqlite3
 
 
 bp = Blueprint("main", __name__, url_prefix='/')
+DB_FILE = os.environ.get("DB_FILE")
+
 
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute("""
+        SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        ORDER BY start_datetime;
+        """)
+        data = curs.fetchall()
+        rows = []
+        for row in data:
+            start = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+            end = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S')
+            rows.append((row[0], row[1], start, end))
+        print(rows)
+    return render_template("main.html", rows=rows)
